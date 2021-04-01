d74d4014edfee228a07dbc637ea53779b4222811
thiago <ithiagolessa@gmail.com>
2021-04-01 07:47:59 -0300

    criação da tabela de Participantes

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Participantes from "./Participantes";
 import Chave from "./Chave";
+import "../assets/css/index.css";
 
 const App = () => {
   return (
--- src/views/Participantes.jsx
+++ src/views/Participantes.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../assets/css/participantes.css";
 
 class Participantes extends Component {
   constructor() {
@@ -7,8 +8,29 @@ class Participantes extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="content">
         <h1>Participantes</h1>
+        <button>Adicionar participantes</button>
+        <table className="tabela">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Celular</th>
+              <th>Email</th>
+              <th>Editar</th>
+              <th>Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Thiago</td>
+              <td>(21)96575-0502</td>
+              <td>ithiagolessa@gmail.com</td>
+              <td>editar</td>
+              <td>excluir</td>
+            </tr>
+          </tbody>
+        </table>
         <Link to="/chave">Chaves</Link>
       </div>
     );
