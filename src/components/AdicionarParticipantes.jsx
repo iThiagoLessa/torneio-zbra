84cf3298d9809e050fad9e3ce83b3da8196d48f4
thiago <ithiagolessa@gmail.com>
2021-04-03 00:10:30 -0300

    ações de fechar e abrir modal

--- src/components/AdicionarParticipantes.jsx
+++ src/components/AdicionarParticipantes.jsx
@@ -2,51 +2,58 @@ import React from "react";
 import "../assets/css/adicionarParticipantes.css";
 
 const AdicionarParticipantes = (props) => {
-console.log(props)
+  console.log(props);
   const cadastraPessoa = (e) => {
     e.preventDefault();
     const form = document.forms.cadastro;
     const { nome, email, telefone } = form;
     const participante = {
-        nome: nome.value,
-        email: email.value,
-        telefone: telefone.value
-    }
+      nome: nome.value,
+      email: email.value,
+      telefone: telefone.value,
+    };
     props.setParticipante(participante);
   };
+  const close = props.closeModal();
+  const fechar = (e) => {
+    e.preventDefault();
+    close();
+  }
   return (
-    <div className="formulario">
-      <div>
-        <header>
-          <span>Adicionar Participantes</span>
-          <span>fechar</span>
-        </header>
-        <form name="cadastro" onSubmit={(e) => cadastraPessoa(e)}>
-          <div>
-            <label>Nome:</label>
-            <input type="text" name="nome" placeholder="insira seu nome" />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input
-              type="Email"
-              name="email"
-              placeholder="insira seu email válido"
-            />
-          </div>
-          <div>
-            <label>Telefone:</label>
-            <input
-              type="number"
-              name="telefone"
-              placeholder="insira seu telefone"
-            />
-          </div>
-          <footer>
-            <button>Adicionar</button>
-            <button>Fechar</button>
-          </footer>
-        </form>
+    <div id="modal">
+      <div className="formulario">
+        <div>
+          <header>
+            <span>Adicionar Participantes</span>
+            <span onClick={props.closeModal()}>fechar</span>
+          </header>
+          <form name="cadastro" onSubmit={(e) => cadastraPessoa(e)}>
+            <div>
+              <label>Nome:</label>
+              <input type="text" name="nome" placeholder="insira seu nome" />
+            </div>
+            <div>
+              <label>Email:</label>
+              <input
+                type="Email"
+                name="email"
+                placeholder="insira seu email válido"
+              />
+            </div>
+            <div>
+              <label>Telefone:</label>
+              <input
+                type="number"
+                name="telefone"
+                placeholder="insira seu telefone"
+              />
+            </div>
+            <footer>
+              <button onClick={e => fechar(e)}>Fechar</button>
+              <button>Salvar</button>
+            </footer>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/views/Participantes.jsx
+++ src/views/Participantes.jsx
@@ -5,14 +5,24 @@ import Tabela from "../components/Tabela";
 import AdicionarParticipantes from "../components/AdicionarParticipantes";
 
 const Participantes = (props) => {
+  const openModal = (e) => {
+    const modal = document.getElementById("modal");
+    modal.style.display = "block";
+    modal.style.opacity = 1;
+  }
+  const closeModal = () => {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+    modal.style.opacity = 0;
+  }
   return (
     <div className="content">
       <h1>Participantes</h1>
       <div className="add">
-        <button>Adicionar participantes</button>
+        <button onClick={e => openModal(e)}>Adicionar participantes</button>
       </div>
       <Tabela />
-      <AdicionarParticipantes participantes={props.participantes} setParticipante={props.setParticipante} setReload={props.setReload} reload={props.reload} />
+      <AdicionarParticipantes closeModal={e => closeModal} participantes={props.participantes} setParticipante={props.setParticipante} setReload={props.setReload} reload={props.reload} />
       <Link to="/chave">Chaves</Link>
     </div>
   );
