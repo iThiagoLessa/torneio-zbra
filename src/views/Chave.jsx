3877059523e5c574df06d5fd1d52db9b952f9a41
thiago <ithiagolessa@gmail.com>
2021-04-05 23:01:38 -0300

    criação chaves torneio pt1

--- src/views/Chave.jsx
+++ src/views/Chave.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../assets/css/chave.css";
+import imgTrofeu from "../assets/img/trofeu.png";
 
 const Chave = () => {
   return (
     <div>
-      <h1>Chaves</h1>
-      <Link to="/">Voltar</Link>
+      <h1>Torneio ZBRA - Chaves</h1>
+      <div className="conteudo">
+        <div className="bloco1">
+          <div className="player1">
+            <div>jogador 1</div>
+            <div></div>
+          </div>
+          <div className="player2">
+            <div></div>
+            <div>jogador2</div>
+          </div>
+        </div>
+        <div className="bloco2">
+          <div className="player1">
+            <div>jogador 3</div>
+            <div></div>
+          </div>
+          <div className="player2">
+            <div></div>
+            <div>jogador4</div>
+          </div>
+        </div>
+        <div className="bloco3">
+          <div>vencedor duelo1</div>
+          <div></div>
+        </div>
+        <div className="bloco4">
+          <div></div>
+          <div>vencedor duelo2</div>
+        </div>
+        <div className="bloco5">
+            <div><div></div></div>
+            <div>
+              <img src={imgTrofeu} alt="Campeão!"/>
+            </div>
+        </div>
+      </div>
+      <button><Link to="/">Voltar</Link></button>
     </div>
   );
 };
